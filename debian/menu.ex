7495b90532abb3c853086e1297ccb0de934dea33
Connor Worley <connorbworley@gmail.com>
2016-10-06 07:47:02 -0700

    Modify changelog, control, rules

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(wpilib-cpp-plugin-files):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="wpilib-cpp-plugin-files" command="/usr/bin/wpilib-cpp-plugin-files"
--- debian/wpilib-cpp-plugin-files.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the wpilib-cpp-plugin-files package
-#
-0 4	* * *	root	[ -x /usr/bin/wpilib-cpp-plugin-files_maintenance ] && /usr/bin/wpilib-cpp-plugin-files_maintenance
